dfca9a6eb622fb74bc8d54c3b93eb21b25f5e263
Pradeep Giri <pradeepgiri.pg123@gmail.com>
2021-03-13 09:16:22 +0530

    Game Added

---
+++ Assets/Scripts/Rifle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward * range, Color.red);
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
